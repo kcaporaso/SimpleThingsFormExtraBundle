63cd40876635a73150f8efdd620265f92cb4c62a
Kevin Caporaso <kcaporaso@dmscolor.com>
2016-09-14 14:48:51 -0400

    More 2.8 updates - removing deprecated code

--- Form/Extension/FieldTypeHelpExtension.php
+++ Form/Extension/FieldTypeHelpExtension.php
@@ -3,6 +3,7 @@
 namespace SimpleThings\FormExtraBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
@@ -41,6 +42,6 @@ class FieldTypeHelpExtension extends AbstractTypeExtension
      */
     public function getExtendedType()
     {
-        return 'form';
+        return FormType::class;
     }
 }
